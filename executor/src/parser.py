419cac3b7239d8dc37d310cfee0cee7c751caac7
lucianoScotellaro <scotellaroluciano7@gmail.com>
2026-01-30 22:13:29 +0100

    Add therapy parsing

--- executor/src/parser.py
+++ executor/src/parser.py
@@ -2,12 +2,22 @@ from therapy import Therapy
 
 class Parser:
 
-    @staticmethod
-    def define_actuators_actions(therapy : Therapy) -> dict:
+    OXYGEN_ACTUATOR = "oxygen_flow_regulator"
+    FLUIDS_ACTUATOR = "drip_valve"
+    BETA_BLOCKING_ACTUATOR = "beta_blocking_infusion_pump"
+    ALERT_ACTUATOR = "alert"
 
-        print("[EXECUTOR]: Defining actuators actions based on the given therapy...")
+    @classmethod
+    def define_actuators_actions(cls, therapy : Therapy) -> dict[str, str | float]:
 
+        # Parse actuators actions from therapy object
+        print("[EXECUTOR]: Defining actuators actions based on given therapy...")
+
+        # {"actuatorType":"actionToBeDone"}
         return {
-            "actuatorType":"actionToBeDone", 
+            cls.OXYGEN_ACTUATOR : therapy.get_oxygen(),
+            cls.FLUIDS_ACTUATOR : therapy.get_fluids(),
+            cls.BETA_BLOCKING_ACTUATOR : therapy.get_beta_blocking(),
+            cls.ALERT_ACTUATOR : therapy.get_alert()
         }
 
