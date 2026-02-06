fc24e90765cd099c44017f9a8c1d84109743d2cf
lucianoScotellaro <scotellaroluciano7@gmail.com>
2026-02-06 16:33:01 +0100

    Add system level MQTTHandler

---
+++ handlers/mqtt_handler.py
@@ -0,0 +1,139 @@
+from os import getenv
+
+from typing import Any
+from collections.abc import Callable
+
+import paho.mqtt.client as mqtt
+from paho.mqtt.reasoncodes import ReasonCode
+from paho.mqtt.enums import MQTTProtocolVersion
+
+class MQTTHandler:
+
+    MQTT_HOSTNAME = getenv("MQTT_HOSTNAME")
+    MQTT_PORT = int(getenv("MQTT_PORT"))
+
+    @classmethod
+    def get_client(cls, client_id : str, subscribe_topics : str | list[str]) -> mqtt.Client:
+
+        # Initialize instance
+        print(f"[{client_id.upper()}]: Initializing MQTT client...")
+
+        ## Set MQTTProtocolVersion 5 for reason codes and authentication support.
+        ## Set CallbackAPIVersion.VERSION2 as VERSION1 deprecated.
+        client = mqtt.Client(
+            client_id = client_id,
+            protocol = MQTTProtocolVersion.MQTTv5,
+            callback_api_version = mqtt.CallbackAPIVersion.VERSION2
+        )
+
+        # Setup
+
+        ## Set callbacks
+        client.on_connect = cls.on_connect
+        client.on_subscribe = cls.on_subscribe
+        client.on_disconnect = cls.on_disconnect
+
+        ## Set subscribe topics
+        client.user_data_set({
+            "subscribe_topics" : subscribe_topics
+        })
+
+        # Print result
+        print(f"[{client_id.upper()}]: MQTT client initialization succeeded.")
+
+        # Return instance
+        return client
+    
+    @staticmethod
+    def set_on_message(
+        client : mqtt.Client,
+        on_message_callback : Callable[[mqtt.Client, dict[str, Any], mqtt.MQTTMessage], None]) -> None:
+        client.on_message = on_message_callback
+
+    @classmethod
+    def connect(cls, client : mqtt.Client) -> None:
+
+        # Connect client with broker
+        print(f"[{client._client_id.upper()}]: Connecting to MQTT broker...")
+        client.connect(
+            cls.MQTT_HOSTNAME,
+            cls.MQTT_PORT
+        )
+
+        # Start network loop on main thread
+        client.loop_start()
+
+    @staticmethod
+    def on_connect(client : mqtt.Client, userdata : dict[str, Any], flags, reason_code : ReasonCode, properties) -> None:
+
+        connection_result = "succeeded" if not reason_code.is_failure else "failed"
+        print(f"[{client._client_id.upper()}]: Connection to MQTT broker {connection_result}. Reason code: {reason_code}.")
+
+        # Subscribe on connection succeeded to handle reconnection scenarios
+        if connection_result == "succeeded":
+
+            topics = userdata["subscribe_topics"]
+
+            # Build subscriptions: (topic, QoS)
+            # Handle multiple topics scenario
+            subscriptions = (topics, 2) if not isinstance(topics, list) else [(topic, 2) for topic in topics]
+
+            print(f"[{client._client_id.upper()}]: Subscribing to {topics if not isinstance(topics, list) else ", ".join(topics)}...")
+            client.subscribe(
+                topic = subscriptions,
+            )
+
+    @staticmethod
+    def publish(client : mqtt.Client, messages : list[ tuple[str, Any] ]) -> None:
+
+        # Send messages
+        # Store sent messages
+        sent : list[ tuple[str, mqtt.MQTTMessageInfo] ] = [ 
+            (topic, client.publish(topic = topic, payload = payload))
+            for topic, payload in messages
+        ]
+
+        # Wait for messages to return, either with success or failure.
+        for topic, message in sent:
+            message.wait_for_publish(timeout = 60)
+
+            # Log results
+            result = "succeeded" if message.is_published else "failed"
+            print(f"[{client._client_id.upper()}]: Publish to {topic} {result}. Reason code: {message.rc}.")
+
+    #
+    # Monitoring callbacks
+    #
+    
+    @staticmethod
+    def on_subscribe(client : mqtt.Client, userdata : dict[str, Any], mid, reason_code_list : list[ReasonCode], properties) -> None:
+        
+        # Log subscriptions results
+
+        ## Compute number of subscription successes
+        subscription_successes : int = 0
+
+        ## Build (topic, reason_code) pairs
+        ## Topics subscribed in the same order they are passed
+        topics = userdata["subscribed_topics"] if isinstance(userdata["subscribed_topics"], list) else [userdata["subscribed_topics"]]
+        for topic, reason_code in zip(topics,reason_code_list):
+
+            ## Log single subscriptions results
+            subscription_result = "succeeded" if not reason_code.is_failure else "failed"
+            print(f"[{client._client_id.upper()}]: Subscription to {topic} {subscription_result}. Reason code: {reason_code}.")
+
+            if not reason_code.is_failure:
+                subscription_successes += 1
+
+        ## Log summary
+        print(f"[{client._client_id.upper()}]: Successfully subscribed to {subscription_successes} topics out of {len(topics)} passed.")
+
+    @staticmethod
+    def on_disconnect(client : mqtt.Client, userdata : dict[str, Any], disconnect_flags, reason_code : ReasonCode, properties) -> None:
+
+        if not reason_code == "Normal disconnection":
+            print(f"[{client._client_id.upper()}]: Unexpected disconnection. Reason code: {reason_code}")
+            return
+        
+        print(f"[{client._client_id.upper()}]: Disconnected.")
+        
